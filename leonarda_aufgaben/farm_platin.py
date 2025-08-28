64247968ed9630731bdca7732d408eff5c5e7278
noe <noefarese@gmx.ch>
2025-08-28 22:49:06 +0200

    set max limit for travel

--- leonarda_aufgaben/farm_platin.py
+++ leonarda_aufgaben/farm_platin.py
@@ -5,8 +5,10 @@ import laser
 if __name__ == "__main__":
     try:
         # Platin Mountain coords: 49989, 76828
-        # TODO: cargo hold check und verkaufen bei core station wenn cargo voll implementieren
+        # TODO: cargo hold check und verkaufen bei core station wenn cargo voll ist implementieren
+        energy.set_limit_normal()
         navigation.travel_position_until_recive(49989, 77000)
+
         energy.mine()
         laser.aim_laser()
     except KeyboardInterrupt:
