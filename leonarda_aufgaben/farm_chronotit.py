739e9f4ab77008198c33f3fac3974006bbe2f758
noe <noefarese@gmx.ch>
2025-09-01 16:32:21 +0200

    add todo

--- leonarda_aufgaben/farm_chronotit.py
+++ leonarda_aufgaben/farm_chronotit.py
@@ -2,6 +2,7 @@ import navigation
 import energy
 import laser
 
+# TODO: braucht massenstabilisator sonst verfällt das chronotit zu nuklearem abfall
 navigation.travel_position_until_recive(-44850, 45728)
 energy.mine()
 laser.aim_laser()
