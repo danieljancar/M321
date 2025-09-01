b00ed6a1dbc39c51da6e9847d5bf4fc469bafe2a
noe <noefarese@gmx.ch>
2025-09-01 09:06:48 +0200

    zwischenstände idk

---
+++ leonarda_aufgaben/phantom_station_nft_hack.py
@@ -0,0 +1,3 @@
+import navigation
+
+navigation.travel_position_until_recive(-60000, 9000)
---
+++ shield_generator.py
@@ -0,0 +1,59 @@
+import uuid
+import requests
+import time
+from pymongo import MongoClient
+
+def trigger_measurement_and_store():
+    client = MongoClient("mongodb://theship:theship1234@10.255.255.254:2021/theshipdb")
+    db = client["theshipdb"]
+    collection = db["vacuum-energy"]
+
+    random = str(uuid.uuid4())
+
+    trigger_url = "http://10.255.255.254:2037/trigger_measurement"
+    data = {"request_id": random}
+    response = requests.post(trigger_url, json=data)
+
+    if response.status_code == 201:
+        print("Messung erfolgreich")
+    else:
+        print(f"Fehler beim Anfordern der Messung: {response.text}")
+        return
+
+    measurement_url = "http://10.255.255.254:2037/measurements/" + random
+    while True:
+        response = requests.get(measurement_url)
+        result = response.json()
+
+        print(result)
+
+        if result["state"] == "measured":
+            print("Messung fertig")
+            break
+        else:
+            print("messung läuft...")
+            time.sleep(2)
+
+
+    result_to_return = ""
+    if "result" in result:
+        collection.delete_many({})
+        result_to_return = result["result"]
+        collection.insert_one({
+            "data": result_to_return
+        })
+        print("messungsergebnis in db gespeichert")
+    else:
+        print("kein messungsergebnis vorhanden")
+
+    delete_response = requests.delete(measurement_url)
+    print(delete_response)
+    if delete_response.status_code == 200:
+        print("messung erfolgreich gelöscht")
+    else:
+        print("fehler beim löschen der messung")
+
+    client.close()
+    return result_to_return
+
+trigger_measurement_and_store()
