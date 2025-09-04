412081d4e2229e490022c4a18fdba397aac6c947
noe <noefarese@gmx.ch>
2025-09-04 16:01:55 +0200

    add energy handling

--- leonarda_aufgaben/farm_chronotit.py
+++ leonarda_aufgaben/farm_chronotit.py
@@ -2,7 +2,19 @@ import navigation
 import energy
 import laser
 
-# TODO: braucht massenstabilisator sonst verfällt das chronotit zu nuklearem abfall
-navigation.travel_position_until_recive(-44850, 45728)
-energy.mine()
+navigation.travel_position_until_recive(-44900, 45728)
+energy.set_limits({
+    "thruster_back": 0.2,
+    "thruster_front": 0.2,
+    "thruster_bottom_left": 0.2,
+    "thruster_front_right": 0.2,
+    "thruster_bottom_right": 0.2,
+    "thruster_front_left": 0.2,
+    "laser": 1,
+    "scanner": 0,
+    "sensor_void_energy": 0,
+    "sensor_antimatter": 0,
+    "sensor_atomic_field": 1,
+    "matter_stabilizer": 1
+})
 laser.aim_laser()
