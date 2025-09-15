a8474c99659498e06065ab7891f54193d138460c
noé <noefarese@gmx.ch>
2025-09-15 09:14:54 +0200

    update mechatrix

--- mechatrix_velox_aufgaben/mechatrix.py
+++ mechatrix_velox_aufgaben/mechatrix.py
@@ -5,5 +5,4 @@ import energy
 Braucht Schildgenerator 
 """
 
-energy.set_limit_normal()
-navigation.travel_position_until_recive(142000, 142000)
+navigation.travel_position_until_recive(150000, 150000)
