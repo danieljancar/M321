152bc3aff9c5084d99fd9f0b25609b264cbd49be
Daniel Jancar <danieljancar@bluewin.ch>
2025-10-13 11:25:42 +0200

    farm

--- leonarda_aufgaben/farm_platin1.py
+++ leonarda_aufgaben/farm_platin1.py
@@ -4,8 +4,9 @@ import laser
 
 if __name__ == "__main__":
     try:
-        navigation.travel_position(49989, 76728)
+        navigation.travel_position(49989, 76600)
         laser.aim_laser()
+        laser.activate()
 
     except KeyboardInterrupt:
         laser.deactive()
