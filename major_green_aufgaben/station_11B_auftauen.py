d757c5c9d926499a3dcab2a178f0846ed4872482
Daniel Jancar <danieljancar@bluewin.ch>
2025-10-16 09:57:57 +0200

    changes

--- major_green_aufgaben/station_11B_auftauen.py
+++ major_green_aufgaben/station_11B_auftauen.py
@@ -2,7 +2,7 @@ import navigation
 import laser
 import time
 
-navigation.travel_position_until_recive(18910, 5536)
+navigation.travel_position_until_recive(18910, 5456)
 
 while True:
     laser.activate()
